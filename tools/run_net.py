c9ab27136cbe6369cfd440b24e8557065bf81dc0
Ali Mottaghi <mottaghi@stanford.edu>
2023-02-27 02:26:36 -0800

    swin128

--- tools/run_net.py
+++ tools/run_net.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-
 """Wrapper to train and test a video classification model."""
+import os
 from slowfast.config.defaults import assert_and_infer_cfg
 from slowfast.utils.misc import launch_job
 from slowfast.utils.parser import load_config, parse_args
 
-# from demo_net import demo
 from test_net import test
 from train_net import train
 from visualization import visualize
@@ -55,11 +54,6 @@ def main():
         or cfg.TENSORBOARD.WRONG_PRED_VIS.ENABLE
     ):
         launch_job(cfg=cfg, init_method=args.init_method, func=visualize)
-
-    # Run demo.
-    if cfg.DEMO.ENABLE:
-        demo(cfg)
-
 
 if __name__ == "__main__":
     main()
